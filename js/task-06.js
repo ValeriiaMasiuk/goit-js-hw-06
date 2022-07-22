d30199f3441b9f34422865dc01a0546b4f22e379
Valeriia Masiuk <valmas1997@gmail.com>
2022-07-22 19:10:13 +0300

    adds changes to task 6

--- js/task-06.js
+++ js/task-06.js
@@ -7,8 +7,10 @@ function onInputBlur(event) {
 
     if (password.length == input.getAttribute('data-length')) {
         input.classList.add('valid');
+        input.classList.remove('invalid')
         return 
     } 
+        input.classList.remove('valid');
         input.classList.add('invalid')
 }
 
