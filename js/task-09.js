9157cc1c87f0dc482fd717b14395b990aa1db78d
Valeriia Masiuk <valmas1997@gmail.com>
2022-07-22 16:38:50 +0300

    adds task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const window = document.querySelector('body')
+const button = document.querySelector('button')
+const color = document.querySelector('.color')
+
+button.addEventListener('click', onBodyColorChange)
+
+function onBodyColorChange() {
+  window.style.backgroundColor = `${getRandomHexColor()}`
+  color.textContent = `${getRandomHexColor()}`;
+}
