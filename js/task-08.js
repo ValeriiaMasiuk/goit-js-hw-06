d6b486180aca046e5cef1ed25d7b7c8aafb2d8ec
Valeriia Masiuk <valmas1997@gmail.com>
2022-07-22 16:23:49 +0300

    adds task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form')
+// console.log(form)
+
+form.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    if (email === '' || password === '') {
+        alert(`Pls, fill all the fields`)
+        console.log(`Form not submitted`)
+        return;
+    } 
+    
+    console.log(`Form submitted`)
+
+    const formData = {
+        email,
+        password,
+    }
+    
+    console.log(formData)
+
+    form.reset()
+}
 
